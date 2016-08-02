d4f38f384061987313add923bf501416ceaaec8f
Julia Campbell <syliesox@gmail.com>
2016-08-02 02:28:33 +0000

    Added more specifics to the notification mailer, such as the specific place that has been commented on.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,12 @@
 class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
     mail(
-      to: "syliesox@gmail.com",
-      subject: "A comment has been added to your place"
+      to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name}"
     )
   end
 
