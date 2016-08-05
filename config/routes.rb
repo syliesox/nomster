dd885a8efaaf82e3b5ea953ba555562f81b1c2c4
Julia Campbell <syliesox@gmail.com>
2016-08-05 00:07:26 +0000

    Added the user dahsboard controller and view pages, as well as linked the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
     resources :photos, only: :create
   end
 
+  resources :users, only: :show
 
 
   # The priority is based upon order of creation: first created -> highest priority.
